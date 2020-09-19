2b7572f6af9f697c2b5345dd679a8370ead6264c
Alcalyxz <jposadasepulveda@gmail.com>
2020-09-19 02:00:09 -0500

    Subiendo Trabajo 1 Empresariales

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <Toolbar />
+
+    <v-main>
+      <Search v-on:searchResult="searchResult" />
+      <Cards v-bind:items="items" />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Toolbar from "./components/Toolbar";
+import Search from "./components/Search";
+import Cards from "./components/Cards";
+
+export default {
+  name: "App",
+
+  components: {
+    Toolbar,
+    Search,
+    Cards,
+  },
+  methods: {
+    searchResult(response) {
+      this.items = response;
+    },
+  },
+  data() {
+    return {
+      items: [],
+    };
+  },
+};
+</script>
